03c60cda5e07f94c8e6c6817c6da56fcb2d56aee
shota-nukumizu <author2000.1225@gmail.com>
2021-12-31 08:25:41 +0900

    provisionally created

--- svelte-site/src/App.svelte
+++ svelte-site/src/App.svelte
@@ -21,21 +21,18 @@
 
 	<h3>{pageName}</h3>
 
+	{#each memoList as item}
 	<div class="card">
 		<div class="card-content">
-			<div class="msg-header">
-				
-				<span class="msg-timestamp"></span>
-				<span class="msg-attachment"><i class="fa fa-paperclip"></i></span>
-			</div>
 			<div class="msg-subject">
-				<span class="msg-subject"><strong id="fake-subject-1"></strong></span>
+				<span class="msg-subject"><strong id="fake-subject-1">{item.title}</strong></span>
 			</div>
 			<div class="msg-snippet">
-				<p id="fake-snippet-1"></p>
+				<p id="fake-snippet-1">{item.content}</p>
 			</div>
 		</div>
 	</div>
+	{/each}
 </main>
 
 <style>
