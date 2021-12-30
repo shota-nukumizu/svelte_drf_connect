6f6f3dcc8296a597701eb232f64c51b7fa24c2ce
shota-nukumizu <author2000.1225@gmail.com>
2021-12-31 08:00:54 +0900

    a little modified

--- svelte-site/services/Api.js
+++ svelte-site/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosAPI = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1/memo-list/'
+    baseURL: 'http://127.0.0.1:8000/api/v1/'
 })
 
 const apiRequest = (method, url, request) => {
